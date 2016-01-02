48235133af170163ae14fd0541a198fdd97a7bca
shaylh <lachmanovich@gmail.com>
2016-01-02 21:17:24 +0200

    added eslint

--- src/dep-res.js
+++ src/dep-res.js
@@ -1,15 +1,28 @@
 (function () {
-    function resolve(depMap, options) {
-        var result = [];
+    'use strict';
+
+    function removeExcluded(depMap, node, exclude) {
+        depMap[node] = depMap[node].filter(function (item) {
+            return !exclude.includes(item);
+        });
+    }
+
+    function validateDep(depMap, node, dependency) {
+        if (!depMap.hasOwnProperty(dependency)) {
+            throw new Error('"' + node + '" has an unknown dependency "' + dependency + '"');
+        }
+    }
+
+    function validateDepMap(depMap, options) {
         var node;
 
-        validateDepMap(depMap, options);
-
         for (node in depMap) {
-            resolveSpecific(depMap, result, node, [node]);
-        }
+            if (options && options.exclude) {
+                removeExcluded(depMap, node, options.exclude);
+            }
 
-        return result;
+            depMap[node].forEach(validateDep.bind(null, depMap, node));
+        }
     }
 
     function resolveSpecific(depMap, result, dependant, path) {
@@ -29,34 +42,24 @@
         }
     }
 
-    function validateDepMap(depMap, options) {
+    function resolve(depMap, options) {
+        var result = [];
         var node;
 
+        validateDepMap(depMap, options);
+
         for (node in depMap) {
-            if (options && options.exclude) {
-                removeExcluded(depMap, node, options.exclude);
-            }
-
-            depMap[node].forEach(validateDep.bind(null, depMap, node));
+            resolveSpecific(depMap, result, node, [node]);
         }
+
+        return result;
     }
 
-    function removeExcluded(depMap, node, exclude) {
-        depMap[node] = depMap[node].filter(function (item) {
-            return !exclude.includes(item)
-        });
-    }
-
-    function validateDep(depMap, node, dependency) {
-        if (!depMap.hasOwnProperty(dependency)) {
-            throw new Error('"' + node + '" has an unknown dependency "' + dependency + '"');
-        }
-    }
-
+    /*eslint-env node,browser*/
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
         module.exports = resolve;
-    } else if (typeof define === 'function' && define.amd) {
-        define([], function () {
+    } else if (typeof define === 'function' && this.define.amd) {
+        this.define([], function () {
             return resolve;
         });
     } else {
